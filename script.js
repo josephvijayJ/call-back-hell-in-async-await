ceab0bc7af73e9f3679da36aee60b16949ec972b
josephvijayJ <87023714+josephvijayJ@users.noreply.github.com>
2021-09-25 10:16:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let count = 10;
+setTimeout(() => {
+  document.getElementById('display').innerText = count--;
+  setTimeout(() => {
+    document.getElementById('display').innerText = count--;
+    setTimeout(() => {
+      document.getElementById('display').innerText = count--;
+      setTimeout(() => {
+        document.getElementById('display').innerText = count--;
+        setTimeout(() => {
+          document.getElementById('display').innerText = count--;
+          setTimeout(() => {
+            document.getElementById('display').innerText = count--;
+            setTimeout(() => {
+              document.getElementById('display').innerText = count--;
+              setTimeout(() => {
+                document.getElementById('display').innerText = count--;
+                setTimeout(() => {
+                  document.getElementById('display').innerText = count--;
+                  setTimeout(() => {
+                    document.getElementById('display').innerText = count--;
+                    setTimeout(() => {
+                      let wish = document.getElementById('display');
+                      wish.innerText = `Happy 
+                      Independence
+                      Day !`;
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
